01e7333476f9632a4fa0653cf37a8f60828666ea
Thais Mesquita <thaisribeiro.mesquita@gmail.com>
2024-09-14 20:44:17 -0300

    chore: change de backend run port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use("/api/v1", ringRoutes);
 
 sequelize.sync().then(() => {
-  app.listen(3000, () => {
+  app.listen(3001, () => {
     console.log("Courage is the best defense you have right now!");
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3001");
   });
 });
